bbac37c8e6f37ea00c5b0dc35588a5c4e43b2209
Philippe Thierry <phil@reseau-libre.net>
2019-01-29 09:38:28 +0100

    [enhancement] differenciate SPARK activation on ads and adb files

--- tools/gen_list.sh
+++ tools/gen_list.sh
@@ -2,9 +2,9 @@
 
 out=`mktemp`;
 
-echo "================================================,================================================,=========" > $out
-echo "C,Ada,SPARK" >> $out
-echo "================================================,================================================,=========" >> $out
+echo "================================================,================================================,=========,==========" > $out
+echo "C,Ada,SPARK,SPARK(api)" >> $out
+echo "================================================,================================================,=========,==========" >> $out
 
 for i in `find kernel/ -type f -iname '*.c'|sort`; do
     file="";
@@ -27,30 +27,45 @@ for i in `find kernel/ -type f -iname '*.c'|sort`; do
 
     if [ -e "$adadir" ]; then
       adafile=`find $adadir -type f -iname ewok-${file%%.c}.adb -o -iname ${file%%.c}.adb`;
+      adaheaderfile=`find $adadir -type f -iname ewok-${file%%.c}.ads -o -iname ${file%%.c}.ads`;
     else
         adafile=""
     fi
     if [ -n "$adafile" ]; then
         adbf=`basename $adbfile 2>/dev/null`;
+        adbheaderf=`basename $adbfile 2>/dev/null`;
         cat $adafile|grep -i "Spark_Mode => on" >/dev/null 2>&1
         res=$?
+        cat $adaheaderfile|grep -i "Spark_Mode => on" >/dev/null 2>&1
+        res2=$?
     else
         adbf=""
+        adbheaderf=""
         res=1
-    fi; 
+        res2=1
+    fi;
     # delete kernel/ prefix
     adafile=${adafile#*/}
-    echo -n $adafile, >> $out; 
+    adaheaderfile=${adaheaderfile#*/}
+    echo -n $adafile, >> $out;
     if [ $res -eq 0 ]; then
-        echo "**yes**" >> $out;
+        echo -n "**yes**," >> $out;
     elif [ -n "$adafile" ]; then
-        echo "no" >> $out;
+        echo -n "no," >> $out;
+    else
+        echo -n "," >> $out;
+    fi
+    if [ $res2 -eq 0 ]; then
+        echo  "**yes**" >> $out;
+    elif [ -n "$adaheaderfile" ]; then
+        echo  "no" >> $out;
     else
         echo "" >> $out;
     fi
+
 done
 
-echo "================================================,================================================,=========" >> $out
+echo "================================================,================================================,=========,==========" >> $out
 
 cat $out | column -t -s, -n | less -F -S -X -K
 
