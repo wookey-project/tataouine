a914e1f3aa5e96b7f25ddf5421e79b5f829b90b9
Arnauld Michelizza <arnauld.michelizza@ssi.gouv.fr>
2019-02-05 12:04:54 +0100

    [enhancement] indent tool

--- tools/indent.sh
+++ tools/indent.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
-indent --no-tabs -ts4 -i4  -di12 \
--nbad -bap -nbc -bbo -hnl -br -brs -c33 -cd33 -ncdb -ce -ci4 -cli0 -d0 \
--nfc1 -ip0 -l80 -lp -npcs -nprs -npsl -sai -saf -saw -ncs -nsc -sob -nfca -cp33 -ss -il1 $*
+indent	\
+--blank-lines-after-declarations	\
+--blank-lines-after-procedures		\
+--braces-on-if-line		\
+--braces-on-struct-decl-line	\
+--cuddle-do-while		\
+--cuddle-else			\
+--case-indentation4		\
+--no-tabs			\
+--indent-level4			\
+--continuation-indentation4	\
+--no-space-after-function-call-names	\
+--space-after-cast		\
+--space-special-semicolon	\
+--space-after-for		\
+--space-after-if		\
+--space-after-while		\
+--declaration-indentation8	\
+--no-blank-lines-after-commas	\
+--line-length80			\
+--dont-break-function-decl-args	\
+--dont-break-procedure-type	\
+--break-after-boolean-operator	\
+--continue-at-parentheses	\
+--no-space-after-parentheses	\
+--indent-label1			\
+$*
