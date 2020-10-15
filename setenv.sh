f0cbbb3f89dd32330c514d8928658ddb0480e2b9
Arnauld Michelizza <arnauld.michelizza@ssi.gouv.fr>
2020-10-15 10:26:05 +0200

    [enhancement, bugfix] Correcting buggy changes in setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env sh
 
+# Reseting variables
+
+unset WOOKEY_ENV
+unset ADA_RUNTIME
+unset CROSS_COMPILE
+unset ST_FLASH
+unset ST_UTIL
+unset USE_LLVM
+unset CLANG_PATH
+unset JAVA_SC_SDK
+unset PYTHON_CMD
 
 # All the variables below are standard values that can be changed by creating
-# a 'setenv.local.sh' file. 
+# a 'setenv.local.sh' file.
 
 if test -f setenv.local.sh; then
     source setenv.local.sh
 fi
 
 #------------------------------------------------------------------------------
-# WARNING! 
+# WARNING!
 # DO NOT EDIT VARIABLE BELOW. PUT YOUR PREFERENCES IN 'setenv.local.sh'
 #------------------------------------------------------------------------------
 
@@ -34,6 +45,18 @@ then
 fi
 
 #
+# Cross-toolchain prefix
+#
+# WARNING: if you use the Ada toochain, please be sure to use the Ada toolchain C compiler
+# to avoid unexpected link and run problems due to incompatible compilers versions.
+# The AdaCore Ada toolchain uses the 'arm-eabi-' prefix
+#
+if [ -z "$CROSS_COMPILE" ]
+then
+    export CROSS_COMPILE="arm-none-eabi-"
+fi
+
+#
 # st-flash tool path
 #
 # This tool is used to flash STM32 boards. Setting this path is not mandatory
@@ -54,18 +77,6 @@ fi
 if [ -z "$ST_UTIL" ]
 then
     export ST_UTIL="/usr/local/bin/st-util"
-fi
-
-#
-# Cross-toolchain prefix
-#
-# WARNING: if you use the Ada toochain, please be sure to use the Ada toolchain C compiler
-# to avoid unexpected link and run problems due to incompatible compilers versions.
-# The AdaCore Ada toolchain uses the 'arm-eabi-' prefix 
-#
-if [ -z "$CROSS_COMPILE" ]
-then
-    export CROSS_COMPILE="arm-none-eabi-"
 fi
 
 #
@@ -114,32 +125,20 @@ fi
 #
 if [ -z "$PYTHON_CMD" ]
 then
-    echo "Looking for suitable python and deps"
     # Try to guess which python we want to use depending on the installed
     # packages. We need Pyscard, Crypto, and IntelHex
     for i in python python3 python2; do
-      if [ -x "`which $i`" ]; then 
-        PYTHON_CMD=$i
-        MISSING=""
-        for j in smartcard Crypto intelhex; do
-          eval "$i -c 'import $j' 2>/dev/null" || MISSING="$MISSING $j"
-        done
-        if [ -z "$MISSING" ];then 
-          echo "\tFound $PYTHON_CMD and the required deps"
-          break
-        else
-          echo "\tFound $PYTHON_CMD but $MISSING is not available" >&2
-          PYTHON_CMD=""
+        if [ -x "$(which $i)" ]; then
+            MISSING=""
+            for j in smartcard Crypto intelhex; do
+                eval "$i -c 'import $j' 2>/dev/null" || MISSING="$MISSING $j"
+            done
+            if [ -z "$MISSING" ]; then
+                export PYTHON_CMD=$i
+                break
+            fi
         fi
-      else
-          echo "\tFailed to find $i" 
-      fi
-    done  
-    if [ -z "$PYTHON_CMD" ]; then
-      echo "Failed to find working python cmd!" >&2
-    else
-      export PYTHON_CMD
-   fi
+    done
 fi
 
 
@@ -147,82 +146,50 @@ fi
 # End of variables
 #---
 
-
 which ${CROSS_COMPILE}gcc > /dev/null
 if [ $? -ne 0 ]; then
-    echo "ERROR: cross-toolchain '${CROSS_COMPILE}gcc' is not in your path !"
-    echo "please update your PATH variable first by including your cross-toolchain"
-    echo "bin/ directory"
+    echo -e "\e[41mCross-toolchain '${CROSS_COMPILE}gcc' is not in your path !\e[0m"
     return 1
 fi
-
 
 if test "${ADA_RUNTIME}/bin/${CROSS_COMPILE}gcc" != "$(which ${CROSS_COMPILE}gcc)"; then
     echo -e "\e[41mPlease check that your Ada toolchain is in your PATH: $ADA_RUNTIME/bin\e[0m"
 fi
 
-# Sanity check on the chosen python command
-if $PYTHON_CMD -c 'import smartcard, Crypto, intelhex' &>/dev/null;
-then
-    echo "Python sanity check OK"
+if [ -z "$PYTHON_CMD" ]; then
+    echo -e "\e[41mPython must be installed with Smartcard, Crypto and intelhex packages\e[0m"
 else
-    echo -e "\e[41mWARNING: either smartcard, Crypto or intelhex Python package has not been found\e[0m"
-    echo -e "\e[41mwith the selected Python command ... Compilation might fail!\e[0m"
-    echo -e "\e[41mContinue if you know what you are doing\e[0m"
+    if ! $PYTHON_CMD -c 'import smartcard, Crypto, intelhex' &>/dev/null;
+    then
+        echo -e "\e[41mPython packages Smartcard, Crypto or intelhex not found\e[0m"
+    fi
 fi
 
-# Sanity check on the perl command
-if perl -e '' &>/dev/null;
+if ! perl -e '' &>/dev/null;
 then
-    echo "Perl sanity check OK"
-else
-    echo -e "\e[41mWARNING: Perl sanity check fails ... Compilation might fail!\e[0m"
-    echo -e "\e[41mContinue if you know what you are doing\e[0m"
+    echo -e "\e[41mPerl check fails ... Compilation might fail!\e[0m"
 fi
 
-# Sanity check on the Java JDK version
+# Checking the Java JDK version
 # This has been taken from http://eed3si9n.com/detecting-java-version-bash
-# Sanity check on the chosen Java JDK version
-# returns the JDK version.
-# 8 for 1.8.0_nn, 9 for 9-ea etc, and "no_java" for undetected
 jdk_version() (
-  if which java >/dev/null; then
-    java_cmd=java
-  elif [ (-n "$JAVA_HOME") -a (-x "$JAVA_HOME/bin/java") ]; then
-    java_cmd="$JAVA_HOME/bin/java"
-  fi
-  result=$("$java_cmd" -Xms32M -Xmx32M -version 2>&1 | perl -ne '/version\s+"(1\.)?(\d+)/ and print "$2"')
-  echo "${result:-no_java}"
-) 
+    if which java >/dev/null; then
+        java_cmd=java
+    elif [ -n "$JAVA_HOME" -a -x "$JAVA_HOME/bin/java" ]; then
+        java_cmd="$JAVA_HOME/bin/java"
+    fi
+    result=$("$java_cmd" -Xms32M -Xmx32M -version 2>&1 | perl -ne '/version\s+"(1\.)?(\d+)/ and print "$2"')
+    echo "${result:-no_java}"
+)
 
 JAVA_VERSION="$(jdk_version)"
-TERMRED=`tput setaf 1`
-TERMNORM=`tput sgr0`
 
 if [ "$JAVA_VERSION" = "no_java" ]; then
-cat <<EOF
-$TERMRED WARNING: JDK version cannot be detected ...
-  Do you have java JDK (8 to 11) installed?
-  Compilation might fail!$TERNORM
-EOF
-else
-    if [ "$JAVA_VERSION" -le 7 ]; then
-cat <<EOF        
-$TERMRED WARNING: JDK version $JAVA_VERSION is < 8
-  This JDK version is not compatible with
-  the Javacard SDK, compilation might fail!$TERMNORM
-EOF
-    else
-        if [ "$JAVA_VERSION" -ge 12 ]; then
-cat <<EOF         
-$TERMRED WARNING: JDK version $JAVA_VERSION is > 11
-  This JDK version is not compatible with
-  the Javacard SDK, compilation might fail!$TERMNORM
-EOF
-        else
-            echo "Java JDK $JAVA_VERSION OK"
-        fi
-    fi
+    echo -e "\e[41mJDK version cannot be detected... Do you have java JDK (8 to 11) installed?\e[0m"
+elif [ "$JAVA_VERSION" -le 7 ]; then
+    echo -e "\e[41mJDK version $JAVA_VERSION is < 8 This JDK version is not compatible with the Javacard SDK\e[0m"
+elif [ "$JAVA_VERSION" -ge 12 ]; then
+    echo -e "\e[41mJDK version $JAVA_VERSION is > 11 This JDK version is not compatible with the Javacard SDK\e[0m"
 fi
 
 # Let's print you current configuration
@@ -244,7 +211,3 @@ echo " *PYTHON_CMD    = $PYTHON_CMD"
 echo
 echo "========================================================="
 
-
-if  [ -z "$ADA_RUNTIME" ]; then
-    echo "Invalid ADA_RUNTIME! Please check that your Ada toolchain binaries are in your PATH"
-fi
